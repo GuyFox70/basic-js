04ea82fe274468f2e3ef41941516adf76c49cea5
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-10-07 16:54:43 +0500

    feat: begin decide task recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,22 @@
 const CustomError = require("../extensions/custom-error");
+let i = -1;
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    console.log(arr);
+
+    function calculate(arr) {
+      i++;
+
+      for (let elem of arr) {
+        if (Array.isArray(elem)) {
+          calculate(elem);
+        }
+      }
+
+      return i;
+    }
+
+    return calculate(arr);
   }
 };
--- test/recursive-depth.test.js
+++ test/recursive-depth.test.js
@@ -9,7 +9,7 @@ const DepthCalculator = require('../src/recursive-depth.js');
 const instance = new DepthCalculator();
 const calculateDepth = instance.calculateDepth.bind(instance);
 
-const createFlatArr = (length) => Array.from({length}, () => Math.floor(Math.random() * length));
+const createFlatArr = (length) => Array.from({ length }, () => Math.floor(Math.random() * length));
 
 describe('Recursive depth', () => {
     //Presence requirement
@@ -24,6 +24,7 @@ describe('Recursive depth', () => {
         it.optional('returns correct depth of flat arrays', () => {
             for (let i = 1; i < 100; i++) {
                 const flatArr = createFlatArr(i);
+                console.log(flatArr);
                 assert.equal(calculateDepth(flatArr), 1);
             }
         });
@@ -32,10 +33,10 @@ describe('Recursive depth', () => {
             assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
             assert.equal(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]), 3);
             assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 5);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 15);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 25);
-            assert.equal(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 31);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 15);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 25);
+            assert.equal(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 31);
         });
         it.optional('works recursively', () => {
             const spy1 = sinon.spy(instance, 'calculateDepth');
