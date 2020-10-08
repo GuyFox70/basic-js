3fdf02bfbf8d5063cb3ee023d22d24052df1b8c2
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-10-08 16:52:14 +0500

    feat: start task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,10 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
+  constructor(bool) {
+    this.dir = bool;
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) throw new Error;
+
+
+  }
   decrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
