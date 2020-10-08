12ebce6c499441e20fb8fe07190f53680599bfda
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-10-08 13:02:06 +0500

    feat: decide task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '' }) {
+  if (typeof (str) != 'string' || typeof (addition) != 'string') {
+    str = String(str);
+    addition = String(addition);
+  }
+
+  let newAdditionStr = '';
+  let result = '';
+
+  for (let i = 1; i <= additionRepeatTimes; i++) {
+    i != additionRepeatTimes ? newAdditionStr = newAdditionStr + (addition + additionSeparator) : newAdditionStr = newAdditionStr + addition;
+  }
+
+  for (let j = 1; j <= repeatTimes; j++) {
+    j != repeatTimes ? result = result + (str + newAdditionStr) + separator : result = result + (str + newAdditionStr);
+  }
+
+  return result;
 };
-  
