af9933ca5785adc15158d8a1e944c600cca8a927
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-10-09 13:50:20 +0500

    feat: start task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let num = parseFloat(sampleActivity);
+
+  Math.floor(Math.log(15 / num) / (0.693 / 5730))
 };
+
+// console.log(Math.ceil(Math.log(15 / 9.122605776326203) / (0.693 / 5730)));
+// console.log(Math.floor(Math.log(15 / 9.122605776326203) / (0.693 / 5730)));
